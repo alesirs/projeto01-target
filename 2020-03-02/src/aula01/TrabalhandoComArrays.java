aaa8ff3eebb4ccaae5d7c390d0f3bfe211647117
Jean Alesi <jpdrlik@gmail.com>
2020-03-06 22:36:27 -0300

    Loops

---
+++ 2020-03-02/src/aula01/TrabalhandoComArrays.java
@@ -0,0 +1,58 @@
+package aula01;
+
+public class TrabalhandoComArrays {
+
+	public static void main(String[] args) {
+		TrabalhandoComArrays tca = new TrabalhandoComArrays();
+		tca.executar();
+	}
+
+	private void executar() {
+		Double[] listaDeNotas = new Double[10];
+		listaDeNotas[0] = 2.5;
+        /*
+		int i = 1;
+		while(i < 10) {
+			listaDeNotas[i] = listaDeNotas[i-1] * 2.5;
+			System.out.println(listaDeNotas[i]);
+			i++;
+		}
+		
+		i = 0;
+		double media = 0;
+		while (i < listaDeNotas.length) {
+			media += listaDeNotas[i];
+			i++;
+		}
+		System.out.printf("A media dos valores é: %.2f " , media / listaDeNotas.length);  
+        */
+		//continue: vai para condicao do while
+		//break: interrompe o while		
+		//JAVA : Labels
+		/*
+		outer: while (i < listaDeNotas.length) {
+			int j = 0;
+			
+			while (j < 10) {
+				if (j==2) {
+					break outer;
+				}
+			}
+		}
+		*/
+		
+		for (int j=1 ; j<10 ; j++) {
+			listaDeNotas[j] = listaDeNotas[j-1] * 2.5;			
+			System.out.println(listaDeNotas[j]);
+		}
+
+		double media = 0;
+		for (int j=1 ; j < listaDeNotas.length; j++) {
+			media += listaDeNotas[j];
+		}
+		System.out.printf("A media dos valores é: %.2f " , media / listaDeNotas.length);  
+
+	}
+	
+
+}
