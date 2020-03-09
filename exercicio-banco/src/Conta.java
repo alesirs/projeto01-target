f587bf3374a3b4d51f533bc49d1e17c48040fce4
Jean Alesi <jpdrlik@gmail.com>
2020-03-09 19:55:26 -0300

    09/03/2020 Projeto "exercicio-banco" Orientacao a objetos

---
+++ exercicio-banco/src/Conta.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class Conta {
+	Integer agencia;
+	Integer numero;
+	Double saldo;
+	List<Movimento> Movimentos;
+	
+	public Integer getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(Integer agencia) {
+		this.agencia = agencia;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	public List<Movimento> getMovimentos() {
+		return Movimentos;
+	}
+
+	public void setMovimentos(List<Movimento> movimentos) {
+		Movimentos = movimentos;
+	}
+}
---
+++ exercicio-banco/src/Movimento.java
@@ -0,0 +1,26 @@
+
+public class Movimento {
+	String tipo;
+	Double valor;
+	String descricao;
+
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
