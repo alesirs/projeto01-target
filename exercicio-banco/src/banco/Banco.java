a5145378a0b656d7988f4c1134a7513919705b10
Jean Alesi <jpdrlik@gmail.com>
2020-03-11 19:41:28 -0300

    Ajustes para uso com for each

--- exercicio-banco/src/banco/Banco.java
+++ exercicio-banco/src/banco/Banco.java
@@ -6,14 +6,11 @@ import java.util.List;
 public class Banco {
 
 	private List<Conta> contas = new ArrayList<Conta>();
-	
+
 	public void depositar(Conta contaParaDeposito, Double valor) {
-		
-		for(int i = 0; i < contas.size(); i++) {
-			Conta conta = contas.get(i);
-			
-			if(conta.getNumero().equals(contaParaDeposito.getNumero())) {
-				conta.getMovimentos().add(gerarMovimentacao("Depósito em conta corrente.", valor, 0));
+		for (Conta conta : contas) {
+			if (conta.getNumero().equals(contaParaDeposito.getNumero())) {
+				conta.getMovimentos().add(gerarMovimentacao("Deposito em conta corrente.", valor, 0));
 				break;
 			}
 		}
@@ -24,36 +21,30 @@ public class Banco {
 		movimentoDeDeposito.setTipo(tipo);
 		movimentoDeDeposito.setDescricao(descricao);
 		movimentoDeDeposito.setValor(valor);
-		
-		System.out.println("Gerando movimentação...");
-		
+
+		System.out.println("Gerando movimentacao...");
+
 		return movimentoDeDeposito;
 	}
-	
+
 	public void sacar(Conta contaDeSaque, Double valor) {
-		for(int i = 0; i < contas.size(); i++) {
-			Conta conta = contas.get(i);
-			
+		for(Conta conta : contas) {
 			if(conta.getNumero().equals(contaDeSaque.getNumero())) {
 				conta.getMovimentos().add(gerarMovimentacao("Saque em conta corrente.", valor, 1));
 				break;
 			}
 		}
 	}
-		
+
 	public void setContas(List<Conta> contas) {
 		this.contas = contas;
 	}
 
 	public void listarMovimentacao(Conta c) {
-		for(int i = 0; i < contas.size(); i++) {
-			Conta conta = contas.get(i);
-			
-			if(conta.getNumero().equals(c.getNumero())) {
-				
-				for(int j = 0; j < conta.getMovimentos().size(); j++) {
-					Movimento movimento = conta.getMovimentos().get(j);
-					
+
+		for(Conta conta : contas) {
+			if (conta.getNumero().equals(c.getNumero())) {
+				for(Movimento movimento : conta.getMovimentos()) {
 					if(movimento.getTipo() == 0) {
 						System.out.println("Movimento de deposito de: "+ movimento.getValor());
 					} else if (movimento.getTipo() == 1){
@@ -63,28 +54,59 @@ public class Banco {
 			}
 		}
 	}
+
 	public void imprimirSaldo(Conta contaCorrentista) {
+		//  VERSAO 3
+		for (Conta conta : contas) {
+			if (conta.getNumero().equals(contaCorrentista.getNumero())) {
+				Double saldo = 0.0;	
+				for (Movimento movimento : conta.getMovimentos()) {
+					if (movimento.getTipo() == 0) {
+						saldo += movimento.getValor().doubleValue();
+					} else {
+						saldo -= movimento.getValor().doubleValue();
+					}
+				}
+				System.out.printf("Saldo em conta corrente: %.2f", saldo);
+			}
+		}
+
+		/*
+		//  VERSAO 2
+		Conta cc = (Conta) contas.stream().filter(c->c.getNumero().equals(contaCorrentista.getNumero()));
+	    Double saldo = 0.0;	
+		for (int j=0 ; j<cc.getMovimentos().size() ; j++) {
+			if (cc.getMovimentos().get(j).getTipo() == 0) {
+			   saldo += cc.getMovimentos().get(j).getValor().doubleValue();
+			} else {
+			   saldo -= cc.getMovimentos().get(j).getValor().doubleValue();
+			}
+		}
+		 */
+
+		/*  VERSAO 1
 		for (int i=0 ; i<contas.size();i++) {
 			Conta conta = contas.get(i);
-			
+
 			if (conta.getNumero().equals(contaCorrentista.getNumero())) {
 			    Double saldo = 0.0;	
-				for (int j=0 ; j<=conta.getMovimentos().size() ; j++) {
+				for (int j=0 ; j<conta.getMovimentos().size() ; j++) {
 					if (conta.getMovimentos().get(j).getTipo() == 0) {
-					   saldo = saldo - conta.getMovimentos().get(j).getValor().doubleValue();
+					   saldo += conta.getMovimentos().get(j).getValor().doubleValue();
 					} else {
-  				       saldo = saldo + conta.getMovimentos().get(j).getValor().doubleValue();
+  				       saldo -= conta.getMovimentos().get(j).getValor().doubleValue();
 					}
 				}
 				System.out.printf("Saldo em conta corrente: %.2f", saldo);
 				break;
-				
+
 			}
 		}
+		 */
 	}
-	
+
 	public void listaMovimentos(Correntista correntista) {
-		
+
 		//movimentos.forEach(mov -> System.out.println(mov.getDescricao() + " - " + mov.getTipo() + " - " + mov.getValor() ));
 	}
 
