138334762e72398a93601e482659eb5c098e26c1
alesirs <45953564+alesirs@users.noreply.github.com>
2020-03-13 16:19:19 -0300

    Leitura de dados para identificar tipo de epregado

--- exercicio-banco/src/PrincipalEmpregados.java
+++ exercicio-banco/src/PrincipalEmpregados.java
@@ -9,38 +9,87 @@ import br.target.empregados.Vendedor;
 
 public class PrincipalEmpregados {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Vendedor vendedorVariavel = new Vendedor(0.5, 2500.0);
+        Scanner entradaConsole = new Scanner(System.in);
+        for (int i=0; ; ) {
+            int tipoEmpregado = 0;
+            System.out.println("Informe o tipo de empregado: ");
+            System.out.print("1-Gerente / 2-Vendedor / 3-Instrutor / 0-Sair: ");
+            entradaConsole = new Scanner(System.in);
+            tipoEmpregado = Integer.valueOf(entradaConsole.next());
+
+            switch (tipoEmpregado) {
+                case 0: {
+                    break;
+                }
+                case 1: {
+                    System.out.println("\n-------------Gerente-------------");
+                    Gerente gerente = new Gerente();
+                    System.out.print("Informe o nome do Gerente: ");
+                    gerente.setNome(entradaConsole.next());
+                    System.out.print("Informe a data de nascimento: ");
+                    gerente.setDataNascimento(entradaConsole.next());
+                    System.out.print("Informe o e-mail: ");
+                    gerente.setEmail(entradaConsole.next());
+                    System.out.print("Informe o salario: ");
+                    gerente.setSalario(Double.valueOf(entradaConsole.next()));
+                    System.out.print("Informe o valor da gratificacao: ");
+                    gerente.setGratificacao(Double.valueOf(entradaConsole.next()));
+                    System.out.printf("Recebiveis do Gerente: R$ %.2f\n\n", gerente.calcularRendimentos());
+                    break;
+                }
+                case 2: {
+                    System.out.println("\n-------------Vendedor-------------");
+                    Vendedor vendedor = new Vendedor();
+                    System.out.print("Informe o nome do Vendedor: ");
+                    vendedor.setNome(entradaConsole.next());
+                    System.out.print("Informe a data de nascimento: ");
+                    vendedor.setDataNascimento(entradaConsole.next());
+                    System.out.print("Informe o e-mail: ");
+                    vendedor.setEmail(entradaConsole.next());
+                    System.out.print("Informe o percentual de comissao: ");
+                    //vendedor.setSalario();
+                    vendedor.setComissao(Double.valueOf(entradaConsole.next()));
+                    System.out.print("Informe o valor das vendas: ");
+                    vendedor.setValorDasVendas(Double.valueOf(entradaConsole.next()));
+                    System.out.printf("Recebiveis do Vendedor: R$ %.2f\n\n", vendedor.calcularRendimentos());
+                    break;
+                }
+                case 3: {
+                    System.out.println("\n-------------Instrutor-------------");
+                    Instrutor instrutor = new Instrutor();
+                    System.out.print("Informe o nome do Instrutor: ");
+                    instrutor.setNome(entradaConsole.next());
+                    System.out.print("Informe a data de nascimento: ");
+                    instrutor.setDataNascimento(entradaConsole.next());
+                    System.out.print("Informe o e-mail: ");
+                    instrutor.setEmail(entradaConsole.next());
+                    System.out.print("Informe o salario: ");
+                    instrutor.setSalario(Double.valueOf(entradaConsole.next()));
+                    System.out.print("Informe a quantidade de horas trabalhadas: ");
+                    instrutor.setHorasTrabalhadas(Integer.valueOf(entradaConsole.next()));
+                    System.out.printf("Recebiveis do Instrutor: R$ %.2f\n\n", instrutor.calcularRendimentos());
+                    break;
+                }
+                default:{
+                    System.out.println("Opcao invalida!");
+                }
+            }
+            if (tipoEmpregado == 0) {
+                break;
+            }
+        }
+		/*
+		Vendedor vendedorVariavel = new Vendedor();
 		vendedorVariavel.setNome("Juca Bala");
-		
-		Gerente gerenteVariavel = new Gerente(10000.0, 1500.0);
+
+		Gerente gerenteVariavel = new Gerente();
 		gerenteVariavel.setNome("Gerentao");
-		
-		Instrutor instrutor = new Instrutor(3000.0, 44);
+
+		Instrutor instrutor = new Instrutor();
 		instrutor.setNome("Master");
 
-		Scanner entradaConsole;
-		System.out.println("Informe o tipo de empregado\n");
-		entradaConsole = new Scanner(System.in);
-		
-		switch (Integer.valueOf(entradaConsole.next())) {
-			case 1: {
-				System.out.println("Informe o nome do Gerente\n");
-				  entradaConsole.next();
-			
-			}
-			case 2: {
-				
-			}
-			case 3: {
-				
-			}
-		}
-		
-		
-		
-		
 		List<Empregado> empregados = new ArrayList<Empregado>();
 		empregados.add(vendedorVariavel);
 		empregados.add(gerenteVariavel);
@@ -49,7 +98,7 @@ public class PrincipalEmpregados {
 		for (Empregado empregado : empregados) {
 			System.out.println(empregado.getNome() + " --> " +  empregado.calcularRendimentos()); 
 		}
-		
-	}
+		*/
+    }
 
 }
